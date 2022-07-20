984e31f47628756dc8315b65378ad01419c98322
Gina Marie <ginamarie214@gmail.com>
2022-07-20 00:36:21 -0500

    to be done

---
+++ assets/jscript/script.js
@@ -0,0 +1,7 @@
+//need to do listener for the entire container where the save button is which is labelled
+
+//we want to add an onclick event that saves that event
+//we want to add the event target, which will tell us which button (i.e. id hour-9 vs hour-11) was clicked on
+
+
+function saveText(inputElement)
