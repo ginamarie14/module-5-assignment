0258c7050cde5461f50ef5da926751193ee0bdd8
Gina Marie <44861723+ginamarie14@users.noreply.github.com>
2022-11-21 01:19:28 -0600

    Update script.js

--- assets/jscript/script.js
+++ assets/jscript/script.js
@@ -2,6 +2,9 @@
 
 //we want to add an onclick event that saves that event
 //we want to add the event target, which will tell us which button (i.e. id hour-9 vs hour-11) was clicked on
-
+function currentTime() {
+    const current = moment().format("MM Do YYYY, h:mm:ss a");
+    $("#today").text(current);
+}
 
 function saveText(inputElement)
